299ec3734c586e9b0e020ee1d0138ba46ca50ef1
victor <chrollolucilfer1402@gmail.com>
2023-11-07 20:07:00 +0530

    set up a basic express server with cors and data parser, plus connects to a postgres database

---
+++ server/src/index.ts
@@ -0,0 +1,48 @@
+import express from "express";
+import "dotenv/config.js";
+import { Pool } from "pg";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// parse data from client
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const config = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+};
+
+const pool = new Pool(config);
+
+const runServer = async () => {
+  try {
+    await pool.connect();
+    console.log("connect to database");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on :${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+runServer();
+
+export { pool };
