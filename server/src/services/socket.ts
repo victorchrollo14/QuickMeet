0276ccd319c1c84586b5c8f20fc4058fb31c518b
victor <chrollolucilfer1402@gmail.com>
2023-11-21 16:46:43 +0530

    added chat and rooms functionality in socket.io

--- server/src/services/socket.ts
+++ server/src/services/socket.ts
@@ -15,10 +15,48 @@ const initSocketServer = (server: HttpServerType) => {
     console.log("a user connected", socket.id);
 
     socket.on("join", (params) => {
-      console.log("joined room", params.roomID);
+      const roomID = params.roomID;
+      users[socket.id] = {
+        roomID: roomID,
+      };
+
+      // create a new room if it doesn't exist
+      if (!rooms[roomID]) {
+        rooms[roomID] = {
+          roomID: roomID,
+          users: [],
+        };
+      }
+
+      rooms[roomID].users.push(socket.id);
+      console.log("user added to room", roomID);
+    });
+
+    socket.on("msg-to-server", (params) => {
+      console.log(params);
+      const roomID = params.roomID;
+      const message = params.message;
+      const otherUsers = rooms[roomID].users; // selecting all users from your room
+
+      console.log(otherUsers);
+
+      // sends message to other users in the particular room
+      otherUsers.forEach((user: string) => {
+        if (user !== socket.id) {
+          console.log(user, socket.id);
+          io.to(user).emit("msg-to-client", message);
+        }
+      });
     });
 
     socket.on("disconnect", () => {
+      // iterating on the room keys, then removing the user from the room
+      Object.keys(rooms).map((roomID) => {
+        rooms[roomID].users = rooms[roomID].users.filter(
+          (user: string) => user != socket.id
+        );
+      });
+      delete users[socket.id];
       console.log(socket.id, "user disconnected");
     });
   });
